b549fd0cb79b3ddfd7d00cf64d85d7a7b2ad9d3b
EdricLai <edric.lai25@ycdsbk12.ca>
2024-02-25 00:30:25 -0500

    updated

--- Sketch.java
+++ Sketch.java
@@ -31,30 +31,30 @@ public class Sketch extends PApplet {
    */
   public void draw() {
     // draws hill
-    fill(0,150,0);
-    ellipse(350,650,1400,500);
+    fill(0, 150, 0);
+    ellipse(350, 650, 1400, 500);
 
     // draws moon
-    fill(200,200,200);
-    ellipse(150,150,150,150);
+    fill(200, 200, 200);
+    ellipse(150, 150, 150, 150);
 
     // draws base
-    fill(200,180,150);
+    fill(200, 180, 150);
     rect(200, 350, 300, 150);
-    rect(400,200,50,150);
+    rect(400, 200, 50, 150);
 
     // draws roof
-    fill(180,160,130);
-    triangle(200,350,350,200,500,350);
+    fill(180, 160, 130);
+    triangle(200, 350, 350, 200, 500, 350);
 
     // draws door
-    fill(220,200,170);
-    rect(325,400,50,100);
+    fill(220, 200, 170);
+    rect(325, 400, 50, 100);
 
     // draws windows
-    fill(160,140,110);
-    ellipse(250,400,50,50);
-    ellipse(450,400,50,50);
+    fill(160, 140, 110);
+    ellipse(250, 400, 50, 50);
+    ellipse(450, 400, 50, 50);
   }
 
 }
